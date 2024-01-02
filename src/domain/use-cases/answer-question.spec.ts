dc31ab9104d79a68dcd623c4d15713960d4759a0
Daniel Levi <daniellevi000@gmail.com>
2024-01-02 09:08:28 -0300

    add gitignore

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswerRepository:AnswersRepository  = {
+  async create(answer:Answer){
+    return 
+  }
+} 
+
+
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova Resposta'
+  })
+
+  expect(answer.content).toEqual('Nova Resposta')
+})
