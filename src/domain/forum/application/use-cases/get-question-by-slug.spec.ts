e966b38d23d1f00fca2f771bccf39c28b0617e37
Daniel Levi <daniellevi000@gmail.com>
2024-01-03 10:46:19 -0300

    create use case dele question end test

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,9 +1,7 @@
-import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { Question } from '../../enterprise/entities/question'
+import { makeQuestion } from 'test/factories/make-question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionsRepository:InMemoryQuestionsRepository
 let  sut: GetQuestionBySlugUseCase
@@ -16,11 +14,8 @@ describe('Get Question By Slug', () => {
   
   it('should be able to get a question by slug', async () => {
    
-    const newQuestion  = Question.create({
-      authorId: new UniqueEntityID(),
-      title: 'Example question',
-      slug: Slug.create('example-question'),
-      content: 'Example content'
+    const newQuestion  = makeQuestion({
+      slug: Slug.create('example-question')
     })
 
    await inMemoryQuestionsRepository.create(newQuestion)
@@ -28,7 +23,7 @@ describe('Get Question By Slug', () => {
     const { question } = await sut.execute({
       slug: 'example-question'
     })
-  
+
     expect(question.id).toBeTruthy()
     expect(inMemoryQuestionsRepository.items[0].content).toEqual(question.content)
     expect(inMemoryQuestionsRepository.items[0].title).toEqual(question.title)
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -3,9 +3,14 @@ import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
- async create(question: Question) {
-      this.items.push(question);
-  }
+
+  async  findById(id: string): Promise<Question | null> {
+    const question =  this.items.find(item => item.id.toString() === id)
+    if(!question) {
+     return null
+    }
+    return question
+    }
 
 async  findBySlug(slug: string){
    const question =  this.items.find(item => item.slug.value === slug)
@@ -13,5 +18,14 @@ async  findBySlug(slug: string){
     return null
    }
    return question
+  }
+
+async create(question: Question) {
+    this.items.push(question);
+}
+
+ async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex( item => item.id === question.id)
+    this.items.splice(itemIndex, 1)
   }
 }
