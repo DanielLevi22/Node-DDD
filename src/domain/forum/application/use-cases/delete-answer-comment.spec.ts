150cb9f9f8dc98e91f61f993130d8e6229eb7dce
Daniel Levi <daniellevi000@gmail.com>
2024-01-04 15:03:12 -0300

    create delte anser comment use case

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -0,0 +1,56 @@
+import { InMemoryAnswersCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
+import { makeAnswerComment } from 'test/factories/make-answer-comment'
+
+let inMemoryAnswersCommentsRepository:InMemoryAnswersCommentsRepository
+let  sut: DeleteAnswerCommentUseCase
+describe('Delete question comment', () => {
+  beforeEach(() => {
+    inMemoryAnswersCommentsRepository = new InMemoryAnswersCommentsRepository()
+
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswersCommentsRepository)
+
+})
+  
+  it('should be able dele a question comment', async () => {
+    
+    const answerComment = makeAnswerComment()
+
+
+    await inMemoryAnswersCommentsRepository.create(answerComment)
+
+
+  	 await sut.execute({
+       answerCommentId: answerComment.id.toString(),
+       authorId: answerComment.authorId.toString(),
+    })
+
+    expect(inMemoryAnswersCommentsRepository.items).toHaveLength(0)
+  
+  })
+
+  it('should not be able dele another user question  comment', async () => {
+    
+    const answerComment = makeAnswerComment({
+      authorId: new UniqueEntityID('author-1')
+    })
+
+
+    await inMemoryAnswersCommentsRepository.create(answerComment)
+
+
+  	 
+
+    expect(async () => {
+      return await sut.execute({
+        answerCommentId: answerComment.id.toString(),
+        authorId: 'author-2',
+     })
+    }).rejects.toBeInstanceOf(Error)
+  
+  })
+
+ 
+})
+
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -3,10 +3,21 @@ import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-coment"
 
 export class InMemoryAnswersCommentsRepository implements AnswersCommentsRepository {
   public items: AnswerComment[] = [];
-
+  async  findById(id: string) {
+    const answerComment =  this.items.find(item => item.id.toString() === id)
+    if(!answerComment) {
+     return null
+    }
+    return answerComment
+    }
 
   async create(answerComment: AnswerComment) {
   this.items.push(answerComment);
+  }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex( item => item.id === answerComment.id)
+    this.items.splice(itemIndex, 1)
   }
 
 }
