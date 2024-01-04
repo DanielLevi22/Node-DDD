fa3751e98da1815a6ee430b0e0fcca5347e4d0d7
Daniel Levi <daniellevi000@gmail.com>
2024-01-04 09:43:24 -0300

    create usecase fetch question answers

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,6 +1,6 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
-
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = [];
@@ -12,16 +12,22 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     }
     return answer
     }
+  async findManyByQuestionId( questionId: string, {page }: PaginationParams) {
+    const answers = this.items.filter(item => item.questionId.toString() === questionId)
+    .slice((page -1 ) * 20 , page * 20)
+    return answers
+    }  
 
- async create(answer: Answer) {
-      this.items.push(answer);
-  }
+  async create(answer: Answer) {
+        this.items.push(answer);
+    }
+    
   async save(answer: Answer): Promise<void> {
     const itemIndex = this.items.findIndex( item => item.id === answer.id)
       this.items[itemIndex] = answer
   }
-async delete(answer: Answer): Promise<void> {
-  const itemIndex = this.items.findIndex( item => item.id === answer.id)
-  this.items.splice(itemIndex, 1)
-}
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex( item => item.id === answer.id)
+    this.items.splice(itemIndex, 1)
+  }
 }
