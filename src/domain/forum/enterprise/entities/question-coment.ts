852d5740b9e0d05fa098e517cd3ea7a65c264246
Daniel Levi <daniellevi000@gmail.com>
2024-01-04 10:22:28 -0300

    create entity base comment

--- src/domain/forum/enterprise/entities/question-coment.ts
+++ src/domain/forum/enterprise/entities/question-coment.ts
@@ -1,37 +1,18 @@
 import { Entity } from "@/core/entities/entity";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
+import { Comment, CommentProps } from "./comment";
 
-export interface QuestionCommentProps{
-  authorId: UniqueEntityID
-  answerId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date 
+export interface QuestionCommentProps extends CommentProps{
+  questionId: string;
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps>{
-  get content() {
-    return this.props.content;
-  }
-  get authorId() {
-    return this.props.authorId;
+export class QuestionComment extends Comment<QuestionCommentProps>{
+ 
+  get questionId() {
+    return this.props.questionId
   }
   
-  get createdAt() {
-    return this.props.createdAt;
-  }
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
-  }
   static create(props: Optional<QuestionCommentProps , 'createdAt'>, id?: UniqueEntityID){
     const questionComment = new QuestionComment({
       ...props,
