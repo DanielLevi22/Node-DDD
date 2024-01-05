0dcf63a0031412fb9ae3d5a8679486ee50fdd572
Daniel Levi <daniellevi000@gmail.com>
2024-01-05 16:28:02 -0300

    create test create questions atachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 
@@ -16,11 +17,20 @@ describe('Create question', () => {
       authorId: '1',
       title: 'Pergunta 1',
       content: 'texto qualquer',
+      attachmentsIds: ['1', '2']
     })
   
+
+
     expect(result.isRight()).toBeTruthy()
     expect(inMemoryQuestionsRepository.items[0].id).toEqual(result.value?.question.id)
-  
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') },)
+    ])
+
+    
   })
 })
 
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,6 +3,7 @@ import { Slug } from "./value-objects/slug"
 import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 import { Optional } from "@/core/types/optional"
 import dayjs from "dayjs"
+import { QuestionAttachment } from "./question-attachment"
 
 
 export interface QuestionProps {
@@ -11,6 +12,7 @@ export interface QuestionProps {
   title: string
   content: string
   slug: Slug
+  attachments: QuestionAttachment[]
   createdAt: Date 
   updatedAt?: Date
 }
@@ -26,6 +28,9 @@ export class Question extends AggregateRoot<QuestionProps> {
   
   get createdAt() {
     return this.props.createdAt;
+  }
+  get attachments() {
+    return this.props.attachments;
   }
   get updatedAt() {
     return this.props.updatedAt;
@@ -63,10 +68,16 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.props.bestAnswerId = bestAnswerId;
     this.touch();
   }
-  static create(props: Optional<QuestionProps , 'createdAt' | 'slug'>, id?: UniqueEntityID){
+  
+  set attachments(attachments: QuestionAttachment[]) {
+    this.props.attachments = attachments;
+  }
+
+  static create(props: Optional<QuestionProps , 'createdAt' | 'slug' | 'attachments'>, id?: UniqueEntityID){
     const question = new Question({
       ...props,
       slug: props.slug ?? Slug.createFromText(props.title),
+      attachments: props.attachments ?? [],
       createdAt: props.createdAt ?? new Date()
     }, id);
     return question
